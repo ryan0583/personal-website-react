eb5bfd85e65e211396a0694d744ddc233ef7e9fa
ryan0583 <ryan0583@gmail.com>
2022-03-16 11:29:07 +0000

    Adjustig calendar width and height

--- src/pages/Gigs.js
+++ src/pages/Gigs.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const Gigs = () => {
   return (
-    <div style={{ padding: '50px' }}>
+    <div style={{ paddingTop: '50px' }}>
       <iframe
         title="cal"
         src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&title=Endorphinmachine%20Gigs&src=NG12a2t0c282Z2NkdTc0NmVyYnBkMmluNmdkbGU0cGtAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=aW11ZmEyb2tscTRjcjI4bWI0YzUwZ3E0ajB1aWVrMTVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=YmMxM3FuczUxY2lidWRjdWxuYnIzbDhzcDVqam9rdjZAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23D50000&color=%23E4C441&color=%23EF6C00"
-        style={{ border: 'solid 1px #777' }}
-        width="800"
-        height="600"
+        style={{ border: 'solid 1px #777', width: '90vw', height: '600px' }}
         frameborder="0"
         scrolling="no"
       />
